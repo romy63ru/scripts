a94bd7328ea406bc03653452c3b29978617bfa04
romy63ru <romy63ru@hotmail.com>
2015-03-24 13:35:41 +0000

    change scripts

--- Install-Choco.ps1
+++ Install-Choco.ps1
@@ -1,3 +1,6 @@
 #Install chocolaty to computer
 set-executionpolicy unrestricted
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+mkdir c:\code\github
+cd c:\code\github
+git clone https://github.com/romy63ru/scripts.git
--- Install-DevEnv.ps1
+++ Install-DevEnv.ps1
@@ -1,4 +1,29 @@
 choco install atom
-choco install cmd
+choco install cmder
 choco install far
 choco install fiddler4
+choco install googlechrome
+choco install firefox
+choco install powershell4
+choco install sysinternals
+choco install nuget.commandline
+choco install paint.net
+choco install wget
+choco install curl
+choco install winmerge
+choco install foxitreader
+choco install linqpad4
+choco install dotpeek
+choco install webpi
+choco install pswindowsupdate
+choco install cpu-z
+choco install ilspy
+choco install stylecop
+choco install actysystemstylerule
+choco install graphviz
+
+#choco install logparser
+
+#atom plugins
+apm install minimap
+apm install language-powershell
