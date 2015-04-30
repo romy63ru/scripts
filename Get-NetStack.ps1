14ddf2588b039decee6fc8fbed3ec6789428621b
unknown <AleksandrR@osw-aleksander.prg.ori.local>
2015-04-30 17:32:35 +0200

    Add Net stack github sources

---
+++ Get-NetStack.ps1
@@ -0,0 +1,13 @@
+mkdir c:\code\github
+
+#typescript
+git clone https://github.com/Microsoft/TypeScript 
+git clone https://github.com/Microsoft/TypeScript-Handbook
+npm install tsd -g
+
+
+#net sources
+git clone https://github.com/Microsoft/referencesource
+
+#fsharp
+git clone https://github.com/fsharp/fsharp/
